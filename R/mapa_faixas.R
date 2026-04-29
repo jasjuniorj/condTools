58bacd06c293da67b8e2334b581634001ae4add0
Seu Nome <jasjuniorj@gmail.com>
2026-04-29 07:29:11 -0300

    ajuste função mapa_faixas

--- R/mapa_faixas.R
+++ R/mapa_faixas.R
@@ -2,6 +2,7 @@
 mapa_faixas <- function(base_dados,
                         variavel,
                         Periodo,
+                        codigoibge,
                         nome_legenda = "Taxa de Acompanhamento",
                         ano = 2020) {
 
@@ -9,61 +10,44 @@ mapa_faixas <- function(base_dados,
   library(ggplot2)
   library(geobr)
 
+  # capturar nomes
+  codigoibge <- rlang::as_name(rlang::ensym(codigoibge))
+
   # --- carregar mapas ---
   mun <- read_municipality(year = ano)
-  #mun$code_muni <- substr(mun$code_muni, 1, nchar(mun$code_muni) - 1)
-  #mun$code_muni <- as.numeric(mun$code_muni)
-
   estados <- read_state(code_state = "all", year = ano)
 
   # --- preparar base ---
   df <- base_dados %>%
-    filter(DS_PERIODO==Periodo) %>%
-    right_join(mun, by = "code_muni") %>%
+    filter(DS_PERIODO == Periodo) %>%
+    right_join(mun, by = c(codigoibge = "code_muni")) %>%
     mutate(
       valor = {{ variavel }},
       faixa = cut(valor,
                   breaks = c(0, 50, 60, 70, 80, 90, 95, 100),
                   include.lowest = TRUE,
-                  labels = c("0–50%", "50–60%", "60–70%",
-                             "70–80%", "80–90%", "90–95%", "95-100%"))
+                  labels = c("0-50%", "50-60%", "60-70%",
+                             "70-80%", "80-90%", "90-95%", "95-100%"))
     )
 
   # --- plot ---
   ggplot(df) +
-    geom_sf(aes(fill = faixa, geometry = geom),
-            color = NA) +
-
+    geom_sf(aes(fill = faixa, geometry = geom), color = NA) +
     scale_fill_manual(
-      name = "Taxa de Acompanhamento",
+      name = nome_legenda,
       values = c(
-        "0–50%"   = "#bcffdd",
-        "50–60%"  = "#a4f0a7",
-        "60–70%"  = "#7dd980",
-        "70–80%"  = "#02ba02",
-        "80–90%"  = "#069525",
-        "90–95%"  = "#095747",
+        "0-50%"   = "#bcffdd",
+        "50-60%"  = "#a4f0a7",
+        "60-70%"  = "#7dd980",
+        "70-80%"  = "#02ba02",
+        "80-90%"  = "#069525",
+        "90-95%"  = "#095747",
         "95-100%" = "#053c32"
       ),
       na.value = "#eeeeee",
       drop = FALSE
-    )+
-
-    geom_sf(data = estados,
-            fill = NA,
-            color = "#7F7F7F",
-            size = 0.1) +
-
-    theme_minimal() +
-    theme(
-      legend.position = "right",
-      plot.title = element_text(size = 9, hjust = 0.5),
-      plot.caption = element_text(size = 9, hjust = 0.5),
-      legend.title = element_text(size = 9),
-      legend.text = element_text(size = 14),
-      panel.grid = element_blank(),
-      axis.text = element_blank(),
-      axis.title = element_blank()
-    )
+    ) +
+    geom_sf(data = estados, fill = NA, color = "#7F7F7F", size = 0.1) +
+    theme_minimal()
 }
 
